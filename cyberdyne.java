68f78964922623f499e747ab881bbdbd081b2393
ray-zap <raya.zapata@gmail.com>
2019-01-12 17:38:55 -0800

    tabs to spaces

--- cyberdyne.java
+++ cyberdyne.java
@@ -3,31 +3,31 @@
 
 public class cyberdyne{
 
-	private int human; 
-	private int cyberneticOrganism;
-	private int available;
-	boolean madeMove; 
-	private gameboard game; 
+    private int human;
+    private int cyberneticOrganism;
+    private int available;
+    boolean madeMove;
+    private gameboard game;
 
-/**
-	constructor
-*/
-	public cyberdyne(gameboard play){
-		game = play; 
-		// System.out.println("I'm a cybernetic organism."); 
-	}
-/**
-	terminator() 
-	serves as the starting point for all of the AI of the computer's game play.
-    012
-    345
-    678
-*/
-	public void terminator(){
-		if (game.getMoves() == 2){
+    /**
+     constructor
+     */
+    public cyberdyne(gameboard play){
+        game = play;
+        // System.out.println("I'm a cybernetic organism.");
+    }
+    /**
+     terminator()
+     serves as the starting point for all of the AI of the computer's game play.
+     012
+     345
+     678
+     */
+    public void terminator(){
+        if (game.getMoves() == 2){
             int move = 0;
             //human chose a corner
-			if (game.getSpace(move = 1) == 'X' || game.getSpace(move = 3) == 'X' || game.getSpace(move = 5) == 'X' || game.getSpace(move = 7) == 'X') {
+            if (game.getSpace(move = 1) == 'X' || game.getSpace(move = 3) == 'X' || game.getSpace(move = 5) == 'X' || game.getSpace(move = 7) == 'X') {
                 switch (move) {
                     case 1:
                         game.setSpace(0, 'O');
@@ -43,81 +43,81 @@ public class cyberdyne{
                         break;
                 }
             }
-			//human chose an edge
-			else if (game.getSpace(0) == 'X' || game.getSpace(2) == 'X' || game.getSpace(6) == 'X' || game.getSpace(8) == 'X'){
-                    game.setSpace(4, 'O');
-			}
+            //human chose an edge
+            else if (game.getSpace(0) == 'X' || game.getSpace(2) == 'X' || game.getSpace(6) == 'X' || game.getSpace(8) == 'X'){
+                game.setSpace(4, 'O');
+            }
             //human chose the center
             else game.setSpace(0, 'O');
-		}
+        }
 
-		else systematic();
-	}
-/**
-	systematic() event handler 
-*/
-	private void systematic(){
-		if (wbp(0)){} 		//try to win the game 
-		else if (wbp(1)){}	//try to block the human from winning
-		else if (wbp(2)){}	//plot a winning move 
-		else 
-			wbp(3);			//random spot picker 
-	}
-
-
-/**
-winblockplan wbp
-picks up the data regarding each winning move (a winning move being three positions on the board across
- up and down, each row and column's three positions.) from the gameboard class' API and sends that to be tested in
- finalfrontier if there's an available position in the cross, row or column.
- */
-
-	private boolean wbp(int type){
-		madeMove = false; 
-		for (int i = 0; i < 8; i++){
-			available = game.statusofpartsA(i); 
-			if (available != -1){
-				cyberneticOrganism = game.statusofpartsO(i);
-				human = game.statusofpartsX(i);
-				finalFrontier(type); 
-				if (madeMove) return madeMove; 
-			}
-		}
-		return madeMove; 
-	}
+        else systematic();
+    }
+    /**
+     systematic() event handler
+     */
+    private void systematic(){
+        if (wbp(0)){} 		//try to win the game
+        else if (wbp(1)){}	//try to block the human from winning
+        else if (wbp(2)){}	//plot a winning move
+        else
+            wbp(3);			//random spot picker
+    }
 
 
-/**
-  finalFrontier() event handler 
-  maybe illogical cohesion here
-*/
-	private void finalFrontier(int type){
-		switch (type){
-			case 0: //try to win the game, 
-				if (cyberneticOrganism == 2 ){ // win the game. 
-					game.setSpace(available, 'O');
-					madeMove = true;
-				}
-			break;
-			case 1: //block human from winning.
-				if (human == 2 ){ //block the human from winning
-					game.setSpace(available, 'O'); 
-					madeMove = true;
-					// System.out.println("get to the choppa!");
-				}			
-			break;
-			case 2: //there are no immediate threats of or potential wins, so plan ahead
-				if (cyberneticOrganism == 1 && human == 0){
-					game.setSpace(available, 'O');
-					madeMove = true;
-				}
-			break;
-			case 3: //there is no way to win the game, so just pick a spot. 
-				// if (available < 9){
-					game.setSpace(available, 'O');
-					madeMove = true;
-				// }
-			break;
-		}
-	}
+    /**
+     winblockplan wbp
+     picks up the data regarding each winning move (a winning move being three positions on the board across
+     up and down, each row and column's three positions.) from the gameboard class' API and sends that to be tested in
+     finalfrontier if there's an available position in the cross, row or column.
+     */
+
+    private boolean wbp(int type){
+        madeMove = false;
+        for (int i = 0; i < 8; i++){
+            available = game.statusofpartsA(i);
+            if (available != -1){
+                cyberneticOrganism = game.statusofpartsO(i);
+                human = game.statusofpartsX(i);
+                finalFrontier(type);
+                if (madeMove) return madeMove;
+            }
+        }
+        return madeMove;
+    }
+
+
+    /**
+     finalFrontier() event handler
+     maybe illogical cohesion here
+     */
+    private void finalFrontier(int type){
+        switch (type){
+            case 0: //try to win the game,
+                if (cyberneticOrganism == 2 ){ // win the game.
+                    game.setSpace(available, 'O');
+                    madeMove = true;
+                }
+                break;
+            case 1: //block human from winning.
+                if (human == 2 ){ //block the human from winning
+                    game.setSpace(available, 'O');
+                    madeMove = true;
+                    // System.out.println("get to the choppa!");
+                }
+                break;
+            case 2: //there are no immediate threats of or potential wins, so plan ahead
+                if (cyberneticOrganism == 1 && human == 0){
+                    game.setSpace(available, 'O');
+                    madeMove = true;
+                }
+                break;
+            case 3: //there is no way to win the game, so just pick a spot.
+                // if (available < 9){
+                game.setSpace(available, 'O');
+                madeMove = true;
+                // }
+                break;
+        }
+    }
 }
--- gameboard.java
+++ gameboard.java
@@ -1,286 +1,286 @@
 /**
  class for the gameboard and it's components 
-*/
+ */
 
 
 public class gameboard{
-	private boolean gameover; 
-	private int moves; 
-	private char[] grid;
-	private Statscollection monitor = new Statscollection(); 
+    private boolean gameover;
+    private int moves;
+    private char[] grid;
+    private Statscollection monitor = new Statscollection();
 
-/**
-	constructor 
-*/
-	public gameboard(){
-		gameover = false; 
-		grid = new char[9];
-		for (int i = 0; i < 9; i++){
-			grid[i] = (char)(i + '0'); 
-		}
-		moves = 1; 
-	}
+    /**
+     constructor
+     */
+    public gameboard(){
+        gameover = false;
+        grid = new char[9];
+        for (int i = 0; i < 9; i++){
+            grid[i] = (char)(i + '0');
+        }
+        moves = 1;
+    }
 
-/**
-	reset function for the array, and probably anything else that would need to be reset. 
-*/
-	public void reset(){
-		for (int i = 0; i < 9; i++){
-			grid[i] = (char)(i + '0'); 
-		}
-		moves = 1;
-		gameover = false; 
-		monitor.reset(); 
-	}
+    /**
+     reset function for the array, and probably anything else that would need to be reset.
+     */
+    public void reset(){
+        for (int i = 0; i < 9; i++){
+            grid[i] = (char)(i + '0');
+        }
+        moves = 1;
+        gameover = false;
+        monitor.reset();
+    }
 
-	/**
-	display the current grid state of affairs 
-	*/
-	public void displayGrid(){
-		String torb = "%6c | %1c | %1c\n";
-		String mid =  "    ---+---+---";
-		System.out.printf(torb, grid[0], grid[1], grid[2]);
-		System.out.println(mid);
-		System.out.printf(torb, grid[3], grid[4], grid[5]);
-		System.out.println(mid);
-		System.out.printf(torb, grid[6], grid[7], grid[8]);
-	}
+    /**
+     display the current grid state of affairs
+     */
+    public void displayGrid(){
+        String torb = "%6c | %1c | %1c\n";
+        String mid =  "    ---+---+---";
+        System.out.printf(torb, grid[0], grid[1], grid[2]);
+        System.out.println(mid);
+        System.out.printf(torb, grid[3], grid[4], grid[5]);
+        System.out.println(mid);
+        System.out.printf(torb, grid[6], grid[7], grid[8]);
+    }
 
-/**
-checkWinner() 
-checks the array to see if anyone has won the game yet. 
-*/
-	public void checkWinner(){
-		if (moves > 4){
-			// monitor.check(); 
-			if (gameover){
-				System.out.println("amazing! the " + monitor.winner + " has won the game!"); 
-			}
-		}
-	}
-/**
-	isGameOver() is basically a get function getting gameover 
-*/
-	public boolean isGameOver(){
-		return gameover;
-	} 
-/**
-	itsaTie() returns gameover, after adjusting for checking moves 
-*/
-	public boolean itsaTie(){
-		if (moves > 9){
-			gameover = true;
-		}
-		return gameover;
-	}
+    /**
+     checkWinner()
+     checks the array to see if anyone has won the game yet.
+     */
+    public void checkWinner(){
+        if (moves > 4){
+            // monitor.check();
+            if (gameover){
+                System.out.println("amazing! the " + monitor.winner + " has won the game!");
+            }
+        }
+    }
+    /**
+     isGameOver() is basically a get function getting gameover
+     */
+    public boolean isGameOver(){
+        return gameover;
+    }
+    /**
+     itsaTie() returns gameover, after adjusting for checking moves
+     */
+    public boolean itsaTie(){
+        if (moves > 9){
+            gameover = true;
+        }
+        return gameover;
+    }
 
-	/**
-	::getMoves():: 
-	*/
-	public int getMoves(){
-		return moves;
-	}
+    /**
+     ::getMoves()::
+     */
+    public int getMoves(){
+        return moves;
+    }
 
-	/**
-	:: getSpace() :: - 
-	*/
-	public char getSpace(int place){
-		return grid[place];
-	}
+    /**
+     :: getSpace() :: -
+     */
+    public char getSpace(int place){
+        return grid[place];
+    }
 
-	/**
-	setSpace() used primarily for the computer entry. 
-	*/
-	public void setSpace(int place, char lett){
-		grid[place] = lett;
-		moves++;
-		monitor.check();
-	}
+    /**
+     setSpace() used primarily for the computer entry.
+     */
+    public void setSpace(int place, char lett){
+        grid[place] = lett;
+        moves++;
+        monitor.check();
+    }
 
-	/**
-	validateEntry() validates entry - also sets the array for the position if valid/available for the human moves. 
-	*/
-	public boolean validateEntry(String input){
-		boolean valid = true; 
-		int len = input.length();
-		if (len != 1 || !Character.isDigit(input.charAt(0))){
-			valid = false; 
-			System.out.println("enter only one digit.");
-		}
-		else if (input.charAt(0) < '0' || input.charAt(0) > '8'){
-			valid = false; 
-			System.out.println("Enter only numbers 0 - 8");
-		}
+    /**
+     validateEntry() validates entry - also sets the array for the position if valid/available for the human moves.
+     */
+    public boolean validateEntry(String input){
+        boolean valid = true;
+        int len = input.length();
+        if (len != 1 || !Character.isDigit(input.charAt(0))){
+            valid = false;
+            System.out.println("enter only one digit.");
+        }
+        else if (input.charAt(0) < '0' || input.charAt(0) > '8'){
+            valid = false;
+            System.out.println("Enter only numbers 0 - 8");
+        }
 
-		if (valid){
-			valid = positionAvailability(input);
-			if (!valid){
-				System.out.println("Position is not available.");
-			}
-			else {
-				int place = Integer.parseInt(input);
-				setSpace(place, 'X');
-			}
-		}
-		return valid; 
-	}
+        if (valid){
+            valid = positionAvailability(input);
+            if (!valid){
+                System.out.println("Position is not available.");
+            }
+            else {
+                int place = Integer.parseInt(input);
+                setSpace(place, 'X');
+            }
+        }
+        return valid;
+    }
 
-	/**
-	positionAvailability()
-	*/
-	public boolean positionAvailability(String input){
-		boolean available = false; 
-		int place = Integer.parseInt(input); 
-		if (grid[place] == input.charAt(0)){
-			available = true; 
-		}
-		return available; 
-	}
+    /**
+     positionAvailability()
+     */
+    public boolean positionAvailability(String input){
+        boolean available = false;
+        int place = Integer.parseInt(input);
+        if (grid[place] == input.charAt(0)){
+            available = true;
+        }
+        return available;
+    }
 
-	/**
-	
-	*/
-	public int statusofpartsX(int i){
-		return monitor.naughts(i); 
-	}
-	public int statusofpartsO(int i){
-		return monitor.oughts(i); 
-	}
-	public int statusofpartsA(int i){
-		return monitor.available(i); 
-	}
+    /**
+
+     */
+    public int statusofpartsX(int i){
+        return monitor.naughts(i);
+    }
+    public int statusofpartsO(int i){
+        return monitor.oughts(i);
+    }
+    public int statusofpartsA(int i){
+        return monitor.available(i);
+    }
 
 
-	//inner classes for handling the status of each of the potential win types, ie rows, cols etc. provides dynamic programming to reduce the number of times the grid is traveresed. especially useful for the api of the computer challenger class. 
-	//
-	class Status{
-		public int oughts; 
-		public int naughts; 
-		public int availablePosition; 
+    //inner classes for handling the status of each of the potential win types, ie rows, cols etc. provides dynamic programming to reduce the number of times the grid is traveresed. especially useful for the api of the computer challenger class.
+    //
+    class Status{
+        public int oughts;
+        public int naughts;
+        public int availablePosition;
 
-		Status(){
-			oughts = 0; 
-			naughts = 0; 
-			availablePosition = -1; 
-		}
+        Status(){
+            oughts = 0;
+            naughts = 0;
+            availablePosition = -1;
+        }
 
-	}
+    }
 
-	class Statscollection{
-		int o = 0; 
-		int x = 0;
-		int pa = -1; 
-		Status[] s = new Status[8];
-		int statposition = 0; 
-		public String winner = ""; 
+    class Statscollection{
+        int o = 0;
+        int x = 0;
+        int pa = -1;
+        Status[] s = new Status[8];
+        int statposition = 0;
+        public String winner = "";
 
-		Statscollection(){
-			reset(); 
-		}
+        Statscollection(){
+            reset();
+        }
 
-		public void reset(){
-			for (int i = 0; i < 8; i++){
-				s[i] = new Status(); 
-			}
-		}
+        public void reset(){
+            for (int i = 0; i < 8; i++){
+                s[i] = new Status();
+            }
+        }
 
-		public void check(){
-			statposition = 0; 
-			across(); 
-			rows(); 
-			columns();
-		}
+        public void check(){
+            statposition = 0;
+            across();
+            rows();
+            columns();
+        }
 
-		private void rows(){
-			if (!gameover){ // check the rows  
-				for (int i = 0; i < 9; i++){
-					examineGrid(i); 
-					if ((i + 1) % 3 == 0){
-						testWinner();
-						updateCollection(); 
-						statposition++; 						
-					}
-				}
-			}
-		}
+        private void rows(){
+            if (!gameover){ // check the rows
+                for (int i = 0; i < 9; i++){
+                    examineGrid(i);
+                    if ((i + 1) % 3 == 0){
+                        testWinner();
+                        updateCollection();
+                        statposition++;
+                    }
+                }
+            }
+        }
 
-		private void examineGrid(int i){
-			if (grid[i] == 'O'){
-				o++;
-			}
-			else if (grid[i] == 'X'){
-				x++;
-			}
-			else pa = i; 
-		}
+        private void examineGrid(int i){
+            if (grid[i] == 'O'){
+                o++;
+            }
+            else if (grid[i] == 'X'){
+                x++;
+            }
+            else pa = i;
+        }
 
-		private void updateCollection(){
-			s[statposition].oughts = o; 
-			s[statposition].naughts = x; 
-			if (o + x == 3) s[statposition].availablePosition = -1; 
-			else s[statposition].availablePosition = pa; 
-			o = 0;
-			x = 0;
-			//does pa need to be reset as well? what are the consequences if not?
-		}
+        private void updateCollection(){
+            s[statposition].oughts = o;
+            s[statposition].naughts = x;
+            if (o + x == 3) s[statposition].availablePosition = -1;
+            else s[statposition].availablePosition = pa;
+            o = 0;
+            x = 0;
+            //does pa need to be reset as well? what are the consequences if not?
+        }
 
-		private void testWinner(){
-			if (o == 3){
-				gameover = true;
-				winner = "computer"; 
-			}
-			else if (x == 3){
-				gameover = true;
-				winner = "human";
-			}
-		}
+        private void testWinner(){
+            if (o == 3){
+                gameover = true;
+                winner = "computer";
+            }
+            else if (x == 3){
+                gameover = true;
+                winner = "human";
+            }
+        }
 
-		private void columns(){
-			if (!gameover){ // check vertically 
-				int vertical = 0;
-				do {
-					for (int i = vertical, count = 1; i < 9; i += 3, count++){
-						examineGrid(i); 
-						if (count % 3 == 0){
-							testWinner();
-							updateCollection(); 
-							statposition++; 	
-						}
-					}
-					vertical++;	
-				} while (vertical < 3 && !gameover);
-			}
-		}
+        private void columns(){
+            if (!gameover){ // check vertically
+                int vertical = 0;
+                do {
+                    for (int i = vertical, count = 1; i < 9; i += 3, count++){
+                        examineGrid(i);
+                        if (count % 3 == 0){
+                            testWinner();
+                            updateCollection();
+                            statposition++;
+                        }
+                    }
+                    vertical++;
+                } while (vertical < 3 && !gameover);
+            }
+        }
 
-		private void across(){
-			if (!gameover){ //check across down 
-				for (int i = 0; i < 9; i += 4){
-					examineGrid(i); 
-				}
-				testWinner(); 
-				updateCollection();
-				statposition++; 
-			}
-			if (!gameover){ //check across up
-				for (int i = 2; i < 8; i +=2){
-					examineGrid(i); 
-				}
-				testWinner(); 
-				updateCollection();
-				statposition++; 
-			}
-		}
+        private void across(){
+            if (!gameover){ //check across down
+                for (int i = 0; i < 9; i += 4){
+                    examineGrid(i);
+                }
+                testWinner();
+                updateCollection();
+                statposition++;
+            }
+            if (!gameover){ //check across up
+                for (int i = 2; i < 8; i +=2){
+                    examineGrid(i);
+                }
+                testWinner();
+                updateCollection();
+                statposition++;
+            }
+        }
 
-		public int oughts(int stat){
-			return s[stat].oughts; 
-		}
+        public int oughts(int stat){
+            return s[stat].oughts;
+        }
 
-		public int naughts(int stat){
-			return s[stat].naughts; 
-		}
+        public int naughts(int stat){
+            return s[stat].naughts;
+        }
 
-		public int available(int stat){
-			return s[stat].availablePosition;
-		}
-	}
+        public int available(int stat){
+            return s[stat].availablePosition;
+        }
+    }
 }
